fddbcbeb9a4cff16c2dc5ce45d5c2845c6b118c0
Andrew Yang <ayang312@gmail.com>
2025-02-12 17:04:03 -0500

    imported authRoute into app.js

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
+const authRoutes = require('./routes/authRoute');
 
 // Load environment variables
 dotenv.config();
@@ -13,7 +14,8 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
-
+// Use the imported API Routes
+app.use("/api/auth", authRoutes);
 
 
 
