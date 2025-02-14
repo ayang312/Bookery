31db1abfc5439f2b51bc38b97d13aff84ec0800b
Andrew Yang <ayang312@gmail.com>
2025-02-13 23:45:52 -0500

    imported isLoggedIn middleware and logout function into authRoutes

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,7 +1,9 @@
-const { login, register } = require("../controllers/authController");
+const { login, register, logout } = require("../controllers/authController");
+const { isLoggedIn } = require("../middlewares/authenticate");
 const router = require("express").Router();
 
 router.post("/login", login);
 router.post("/register", register);
+router.post("/logout", isLoggedIn, logout);
 
 module.exports = router;
