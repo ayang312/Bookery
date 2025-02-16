641ba52916257d69a0eeb23df2013ee3cf3d7a2c
Andrew Yang <ayang312@gmail.com>
2025-02-16 15:49:22 -0500

    made some more endpoints to be created for admin

--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -3,6 +3,20 @@ const { getAllUsers } = require('../controllers/adminController');
 const { isLoggedIn } = require('../middlewares/authenticate');
 const { isAdmin } = require("../middlewares/authorize");
 
+// Get all users
 router.get("/users", isLoggedIn, isAdmin, getAllUsers);
+// Create new users
+
+// Update users
+
+// Delete users
+
+// Create timeSlots
+
+// Update timeSlots
+
+// Delete timeSlots
+
+// Delete appointments
 
 module.exports = router;
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -2,8 +2,14 @@ const { login, register, logout } = require("../controllers/authController");
 const { isLoggedIn } = require("../middlewares/authenticate");
 const router = require("express").Router();
 
+// Login
 router.post("/login", login);
+// Register
 router.post("/register", register);
+// Logout
 router.post("/logout", isLoggedIn, logout);
+// OAuth Register
+
+// Password Reset
 
 module.exports = router;
