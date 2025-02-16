4933861076d05803a3bee5b56a30dfa292837e3f
Andrew Yang <ayang312@gmail.com>
2025-02-16 15:13:23 -0500

    imported getAllUsers fxn and isLoggedIn middleware fxn into admin /users route

--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
+const { getAllUsers } = require('../controllers/adminController');
+const { isLoggedIn } = require('../middlewares/authenticate');
 
-router.get("/users");
+router.get("/users", isLoggedIn, getAllUsers);
 
 module.exports = router;
