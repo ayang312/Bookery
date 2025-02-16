8d4361ab59007626cf5d8c44d0ca7fed50700414
Andrew Yang <ayang312@gmail.com>
2025-02-16 15:17:30 -0500

    imported isAdmin middleware fxn into adminRoute

--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const { getAllUsers } = require('../controllers/adminController');
 const { isLoggedIn } = require('../middlewares/authenticate');
+const { isAdmin } = require("../middlewares/authorize");
 
-router.get("/users", isLoggedIn, getAllUsers);
+router.get("/users", isLoggedIn, isAdmin, getAllUsers);
 
 module.exports = router;
