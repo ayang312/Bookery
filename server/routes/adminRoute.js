4b2883cec5099155e458588773498203acb29bc9
Andrew Yang <ayang312@gmail.com>
2025-02-18 19:51:47 -0500

    imported createUser function in the adminRoutes

--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
-const { getAllUsers } = require('../controllers/adminController');
+const { getAllUsers, createUser } = require('../controllers/adminController');
 const { isLoggedIn } = require('../middlewares/authenticate');
 const { isAdmin } = require("../middlewares/authorize");
 
 // Get all users
 router.get("/users", isLoggedIn, isAdmin, getAllUsers);
 // Create new users
+router.post("/users", isLoggedIn, isAdmin, createUser);
 
 // Update users
 
