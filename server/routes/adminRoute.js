0d577aa8aac8c6faa4734c30a10eec96cf3f104a
Andrew Yang <ayang312@gmail.com>
2025-02-18 20:40:54 -0500

    imported updateUser function into adminRoutes

--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -3,16 +3,19 @@ const {
   getAllUsers,
   createUser,
   deleteUser,
+  updateUser,
 } = require("../controllers/adminController");
 const { isLoggedIn } = require("../middlewares/authenticate");
 const { isAdmin } = require("../middlewares/authorize");
 
 // Get all users
 router.get("/users", isLoggedIn, isAdmin, getAllUsers);
+
 // Create new users
 router.post("/users", isLoggedIn, isAdmin, createUser);
 
 // Update users
+router.put("/users/:id", isLoggedIn, isAdmin, updateUser);
 
 // Delete users
 router.delete("/users/:id", isLoggedIn, isAdmin, deleteUser);
