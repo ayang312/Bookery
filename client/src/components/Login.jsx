7e9caa4b0302a2c066c9a8636fddce9d48dcf239
Andrew Yang <ayang312@gmail.com>
2025-02-19 21:40:00 -0500

    created simple login form

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,30 @@
-
-
 const Login = () => {
-    
+  const handleSubmit = () => {};
+
+  return (
+    <>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <h2>Login</h2>
+          <label htmlFor="username">Username/Email</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            required
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            required
+          />
+          <button type="submit"></button>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Login;
