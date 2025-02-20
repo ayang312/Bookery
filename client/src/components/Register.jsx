5a071170f03abfa4fa0cd90299fdf012ea00f83d
Andrew Yang <ayang312@gmail.com>
2025-02-20 18:16:20 -0500

    created formData object in state to send to the backend. created a handleChange function to handle input changes and edited the return values on the input to set the e.target.value to the corresponding keys in the formData object

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
+
 const Register = () => {
   // Handle form data to be sent to backend
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
   // Handle errors
   // Handle loading
-  // Form validations (later)
+  // Form validations... username, email, password (later)
   // Handle the input changes
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
   // Handle the form submission
   // Handle the API logic
   // return a form with inputs
@@ -16,19 +30,43 @@ const Register = () => {
           {/* Username */}
           <div>
             <label htmlFor="username"></label>
-            <input type="text" name="username" id="username" />
+            <input
+              type="text"
+              name="username"
+              id="username"
+              value={formData.username}
+              onChange={handleChange}
+              required
+              placeholder="Enter a new unique username"
+            />
           </div>
 
           {/* Email */}
           <div>
             <label htmlFor="email"></label>
-            <input type="email" name="email" id="email" />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+              placeholder="Enter a new unique email"
+            />
           </div>
 
           {/* Password */}
           <div>
             <label htmlFor="password"></label>
-            <input type="password" />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+              placeholder="Create a new password"
+            />
           </div>
 
           {/* Submit */}
