b830c9705eee8535f388197ddfa07bb70d571832
Andrew Yang <ayang312@gmail.com>
2025-02-19 21:50:58 -0500

    added handleSubmit function to handle form submissions

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,5 +1,46 @@
+import { useState } from "react";
+
 const Login = () => {
-  const handleSubmit = () => {};
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  //   Handle Form submissions
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    setLoading(true);
+
+    // API Call
+    try {
+      const response = await fetch("http://localhost:3000/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message || "Something went wrong fetching API");
+      }
+
+    //   Handle successful login
+    console.log("Login successful", data);
+    alert("Login successful");
+    
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <>
@@ -7,20 +48,10 @@ const Login = () => {
         <form onSubmit={handleSubmit}>
           <h2>Login</h2>
           <label htmlFor="username">Username/Email</label>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            required
-          />
+          <input type="text" name="username" id="username" required />
           <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            required
-          />
-          <button type="submit"></button>
+          <input type="password" name="password" id="password" required />
+          <button type="submit">Login</button>
         </form>
       </div>
     </>
