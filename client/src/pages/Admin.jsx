8146f6c769393ed61a4714dea8edeceaf9e3e749
Andrew Yang <ayang312@gmail.com>
2025-02-27 22:54:43 -0500

    imported useDispatch

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,7 +1,24 @@
+import { useDispatch } from "react-redux";
+
 const Admin = () => {
+const dispatch = useDispatch();
+
+const handleLogout = async () => {
+  try {
+    // Dispatch logout action from authSlice
+
+  } catch (error) {
+    console.error("Failed to logout", error);
+    
+  }
+}
+
   return (
     <>
       <h1>Admin Dashboard</h1>
+      <div>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
     </>
   );
 };
