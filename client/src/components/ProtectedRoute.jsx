cd21f164612869275b127a3f214fb02af1bd3c07
Andrew Yang <ayang312@gmail.com>
2025-02-27 22:13:19 -0500

    edited ProtectedRoute.jsx to navigate to Unauthorized.jsx

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,11 +1,12 @@
 import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-const ProtectedRoute = ({children, requiredRole}) => {
+const ProtectedRoute = ({ children, requiredRole }) => {
   const { user, isAuthenticated } = useSelector((state) => state.auth);
 
+  //   If the user is not authenticated or not admin, navigate to Unauthorized page
   if (!isAuthenticated || user?.role !== requiredRole) {
-    return <Navigate to="/" replace />; // Redirect to home
+    return <Navigate to="/unauthorized" replace />; // Redirect to home
   }
 
   return children; //Render the protected component
