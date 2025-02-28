8d0779fb81c885a18792a1b887bd1a06b7b9ad04
Andrew Yang <ayang312@gmail.com>
2025-02-27 22:56:57 -0500

    redirect back to homepage upon logging out and alert a success message

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,14 +1,19 @@
 import { useDispatch } from "react-redux";
 import { logout } from "../redux/auth/authSlice";
+import { useNavigate } from "react-router-dom";
 
 const Admin = () => {
 const dispatch = useDispatch();
+const navigate = useNavigate();
 
 const handleLogout = async () => {
   try {
     // Dispatch logout action from authSlice
     dispatch(logout());
-
+    // Redirect back to homepage
+    navigate("/");
+    // Show alert message
+    alert("Successfully logged out!");
   } catch (error) {
     console.error("Failed to logout", error);
     
