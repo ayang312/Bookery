2348c2edae483cb79478f7ae85d42b825b18c832
Andrew Yang <ayang312@gmail.com>
2025-03-02 16:32:19 -0500

    added a createNewTimeSlot controller function

--- server/controllers/timeSlotsController.js
+++ server/controllers/timeSlotsController.js
@@ -7,7 +7,22 @@ const getAllTimeSlots = async (req, res, next) => {
     res.status(200).json(timeSlots);
   } catch (error) {
     res.status(500).json({ message: "Failed to fetch all time slots" });
-  };
+  }
 };
 
-module.exports = { getAllTimeSlots };
+// Create a new time slot
+const createNewTimeSlot = async (req, res, next) => {
+  try {
+    const newTimeSlot = await prisma.timeSlot.create({
+      data: {
+        date: new Date(req.body.date),
+        time: req.body.time,
+      },
+    });
+    res.status(201).json(newTimeSlot);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to create new time slot" });
+  }
+};
+
+module.exports = { getAllTimeSlots, createNewTimeSlot };
