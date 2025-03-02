3ac35061af239c42f4a0828a1b761fa2ab668b2f
Andrew Yang <ayang312@gmail.com>
2025-03-02 17:07:13 -0500

    created createNewTimeSlot mutation

--- client/src/redux/timeSlot/timeSlotApi.js
+++ client/src/redux/timeSlot/timeSlotApi.js
@@ -2,11 +2,22 @@ import { rootApi } from "../api/rootApi";
 
 export const timeSlotApi = rootApi.injectEndpoints({
   endpoints: (builder) => ({
-    
     // Fetch all time slots
     getAllTimeSlots: builder.query({
-        query: () => "/time-slots",
+      query: () => "/time-slots",
     }),
 
+    // Create a new time slot
+    createNewTimeSlot: builder.mutation({
+      query: (newTimeSlot) => ({
+        url: "/time-slots",
+        method: "POST",
+        body: newTimeSlot,
+      }),
+      
+    }),
   }),
 });
+
+export const { useGetAllTimeSlotsQuery, useCreateNewTimeSlotMutation } =
+  timeSlotApi;
