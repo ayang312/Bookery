6642d6a3d677ba23d23e3e9bfe30a5e87d400a83
Andrew Yang <ayang312@gmail.com>
2025-03-02 16:24:40 -0500

    made new timeSlotRoute.js with a getAllTimeSlots route

---
+++ server/routes/timeSlotRoute.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+// Get all timeSlots
+router.get("/", getAllTimeSlots);
+
+// Create a new timeSlot
+
+// Update a timeSlot
+
+// Delete a timeSlot
+
+module.exports = router;
