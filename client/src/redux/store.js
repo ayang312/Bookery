81e2296d689eab66fa66e3f49d977709ad791f15
Andrew Yang <ayang312@gmail.com>
2025-03-02 17:12:58 -0500

    imported RTK Query timeSlotApi into store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,14 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authApi } from "./auth/authApi";
 import authReducer from "./auth/authSlice";
+import { timeSlotApi } from "./timeSlot/timeSlotApi";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     [authApi.reducerPath]: authApi.reducer,
+    [timeSlotApi.reducerPath]: timeSlotApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware),
+    getDefaultMiddleware().concat(authApi.middleware, timeSlotApi.middleware),
 });
 
 export default store;
