266a0d6e2b72b0242e14443dcfe4b758be8274cf
Andrew Yang <ayang312@gmail.com>
2025-03-02 16:47:40 -0500

    created updateTimeSlot controller fxn

--- server/controllers/timeSlotsController.js
+++ server/controllers/timeSlotsController.js
@@ -6,7 +6,7 @@ const getAllTimeSlots = async (req, res, next) => {
     const timeSlots = await prisma.timeSlot.findMany();
     res.status(200).json(timeSlots);
   } catch (error) {
-    res.status(500).json({ message: "Failed to fetch all time slots" });
+    res.status(404).json({ message: "Failed to fetch all time slots" });
   }
 };
 
@@ -21,8 +21,32 @@ const createNewTimeSlot = async (req, res, next) => {
     });
     res.status(201).json(newTimeSlot);
   } catch (error) {
-    res.status(500).json({ error: "Failed to create new time slot" });
+    next({ message: "Failed to create new time slot" });
   }
 };
 
-module.exports = { getAllTimeSlots, createNewTimeSlot };
+// Update a time slot
+const updateTimeSlot = async (req, res, next) => {
+  try {
+    const existingTimeSlot = await prisma.timeSlot.findUnique({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!existingTimeSlot) {
+      return res.status(404).json({ message: "Time Slot Not Found." });
+    }
+
+    const updatedTimeSlot = await prisma.timeSlot.update({
+      where: { id: req.params.id },
+      data: { isBooked: req.body.isBooked },
+    });
+
+    res.status(201).json(updatedTimeSlot);
+  } catch (error) {
+    next({ message: "Failed to update time slot" });
+  }
+};
+
+module.exports = { getAllTimeSlots, createNewTimeSlot, updateTimeSlot };
