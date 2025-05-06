45fae44f752d374b8b839497cd75c4ce58f4d60b
Andrew Yang <ayang312@gmail.com>
2025-05-05 20:11:00 -0400

    destructured TimeSlotManagement from AdminDashboard

---
+++ client/src/admin/TimeSlotManagement.jsx
@@ -0,0 +1,19 @@
+const TimeSlotManagement = () => {
+  return (
+    <>
+      {/* Time Slot Management */}
+      <section>
+        <h2>Time Slot Management</h2>
+        <div>
+          <h3>Create New Time Slot</h3>
+          {/* Inputs to create time slots */}
+        </div>
+
+        <h3>All Time Slots</h3>
+        <ul>{/* Map all the time slots here */}</ul>
+      </section>
+    </>
+  );
+};
+
+export default TimeSlotManagement;
