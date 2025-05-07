7a4ba7eb55b7af9b1ed0286294cc832a883de84a
Andrew Yang <ayang312@gmail.com>
2025-05-07 18:19:57 -0400

    created AppointmentTracking component for admin dashboard

---
+++ client/src/admin/AppointmentTracking.jsx
@@ -0,0 +1,15 @@
+const AppointmentTracking = () => {
+  return (
+    <>
+      <section>
+        <h2>Appointment Tracking</h2>
+
+        {/* Create New Appointment */}
+
+        {/* List All Appointments */}
+      </section>
+    </>
+  );
+};
+
+export default AppointmentTracking;
