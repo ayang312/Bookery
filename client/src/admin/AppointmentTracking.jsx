383dc0fb003964f418cc1c50a9ba920801881004
Andrew Yang <ayang312@gmail.com>
2025-05-07 18:53:22 -0400

    added a function to handle creating new appointments, set the newAppointment form to local state.

--- client/src/admin/AppointmentTracking.jsx
+++ client/src/admin/AppointmentTracking.jsx
@@ -1,8 +1,58 @@
-import { useGetAllAppointmentsQuery } from "../redux/admin/appointmentApi";
+import {
+  useCreateNewAppointmentMutation,
+  useGetAllAppointmentsQuery,
+} from "../redux/admin/appointmentApi";
 
 const AppointmentTracking = () => {
   // RTK Query calls
-  const { data: appointments = [] } = useGetAllAppointmentsQuery();
+  const { data: appointments = [], refetchAppointments } =
+    useGetAllAppointmentsQuery();
+  const [createNewAppointment] = useCreateNewAppointmentMutation();
+
+  // Local state for new appointment form
+  const [newAppointment, setNewAppointment] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    eventDate: "",
+    primaryVenue: "",
+    secondaryVenue: "",
+    timeSlotId: "",
+  });
+
+  // Handle creating new appointments
+  const handleCreateNewAppointment = async () => {
+    try {
+      // Ensure that a time slot is selected
+      if (!newAppointment.timeSlotId) {
+        alert("Please select a time slot");
+        return;
+      }
+
+      //   Create the new appointment
+      await createNewAppointment(newAppointment).unwrap();
+      alert("Appointment created successfully!");
+      //   Reset the state
+      setNewAppointment({
+        firstName: "",
+        lastName: "",
+        email: "",
+        phoneNumber: "",
+        eventDate: "",
+        primaryVenue: "",
+        secondaryVenue: "",
+        timeSlotId: "",
+      });
+      //   Refresh list of appointments after mutation
+      refetchAppointments();
+    } catch (error) {
+      console.error("Failed to create new appointment", error);
+    }
+  };
+  // Handle updating appointments
+
+  // Handle deleting appointments
 
   return (
     <>
